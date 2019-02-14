0065e6f04879fa1a338ac80cbef2913c5df0d2fc
Artem Varaksa <aymfst@gmail.com>
2019-02-14 16:43:43 +0300

    Add Response struct

--- src/context.rs
+++ src/context.rs
@@ -1,23 +1,67 @@
-//! The [`Context`] struct.
+//! The [`Context`] and [`Response`] structs.
 
 use crate::{core::Event, request::Object};
 use rvk::APIClient;
 use std::sync::{Arc, Mutex};
 
-/// Stores information necessary for handlers, manages the bot's current
-/// response to the message/event it is associated with, and provides convenient
-/// ways for modifying that as well as sending the resulting message.
+/// Stores information necessary for handlers, allows to send the resulting
+/// message.
 #[derive(Debug)]
 pub struct Context {
     event: Event,
     object: Object,
     api: Arc<Mutex<APIClient>>,
-    // TODO
+    response: Response,
 }
 
 impl Context {
     /// Creates a new [`Context`].
     pub fn new(event: Event, object: Object, api: Arc<Mutex<APIClient>>) -> Self {
-        Self { event, object, api }
+        Self {
+            event,
+            object,
+            api,
+            response: Response::new(),
+        }
+    }
+
+    /// Returns the event type that caused this handler to run.
+    pub fn event(&self) -> Event {
+        self.event
+    }
+
+    /// Returns the object associated with the event (given by Callback API).
+    pub fn object(&self) -> &Object {
+        &self.object
+    }
+
+    /// Returns an [`rvk::APIClient`], wrapped into
+    /// [`Arc`][`std::sync::Arc`]`<`[`Mutex`][`std::sync::Mutex`]`<...>>`.
+    pub fn api(&self) -> Arc<Mutex<APIClient>> {
+        Arc::clone(&self.api)
+    }
+
+    /// Returns the current pending response object.
+    pub fn response(&mut self) -> &mut Response {
+        &mut self.response
+    }
+}
+
+/// Manages the bot's current response to a message/event
+#[derive(Debug)]
+pub struct Response {
+    // TODO
+}
+
+impl Default for Response {
+    fn default() -> Self {
+        Self {}
+    }
+}
+
+impl Response {
+    /// Creates a new [`Response`].
+    pub fn new() -> Self {
+        Default::default()
     }
 }
