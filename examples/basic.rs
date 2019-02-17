4c28444a63dd5d501954e4085a9a336dfa94930f
Artem Varaksa <aymfst@gmail.com>
2019-02-17 14:50:15 +0300

    Improve example

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,8 +1,11 @@
-use vk_bot::{Bot, Core, Event, Handler, Tester};
+use vk_bot::{
+    keyboard::{Button, Color, Keyboard},
+    Bot, Core, Event, Handler, Tester,
+};
 
 fn main() {
     // A simple closure for convenience...
-    let simple_message_handler = |message| {
+    let simple_handler = |message| {
         // ...that returns a handler!
         Handler::new(move |ctx| {
             ctx.response().set_message(message);
@@ -10,31 +13,30 @@ fn main() {
         })
     };
 
+    // Create a keyboard.
+    let kbd = Keyboard::new(
+        vec![vec![
+            Button::new("A", Color::Primary, None),
+            Button::new("B", Color::Default, Some(r#"{"a": "b"}"#.into())),
+        ]],
+        false,
+    );
+
     let core = Core::new()
         .cmd_prefix("/")
-        .cmd("info", simple_message_handler("I am a VK bot"))
-        .regex("nice", simple_message_handler("Thanks!"))
-        .on(
-            Event::NoMatch,
-            simple_message_handler("Idk how to respond!"),
-        )
-        .on(
-            Event::Start,
+        .cmd(
+            "keyboard",
             Handler::new(|ctx| {
-                ctx.response().set_message("Started!");
-                // ctx.response().set_keyboard(...);
-
+                ctx.response().set_message("Here you go:");
+                ctx.response().set_keyboard(kbd);
                 eprintln!("{:#?}", ctx.send());
             }),
         )
-        .payload(
-            r#"{"a":"b"}"#,
-            simple_message_handler("Received {\"a\": \"b\"} in payload!"),
-        )
-        .dyn_payload(
-            Tester::new(|_| true), // accept all remaining payloads!
-            simple_message_handler("Received a payload!"),
-        );
+        .regex("nice", simple_handler("Thanks!"))
+        .on(Event::NoMatch, simple_handler("I don't understand..."))
+        .payload(r#"{"a":"b"}"#, simple_handler("You pressed button B!"))
+        // accept all remaining payloads!
+        .dyn_payload(Tester::new(|_| true), simple_handler("Received a payload!"));
 
     Bot::new(
         "your vk token",      // VK token
